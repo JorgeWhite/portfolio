e17aa719751c67452e8cf37d43e6b790d0d6949a
Jorge <jorge.aguilar@c3.unam.mx>
2026-01-21 19:05:01 -0600

    Initial portfolio structure

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Backend is running 🚀"}
+
+@app.get("/hello")
+def hello():
+    return {"hello": "Hello from FastAPI"}
+
